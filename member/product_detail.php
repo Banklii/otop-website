239966d0cdaf7a0310d9ed546ebe196d1c50601c
Banklii <naybanksorryza123@gmail.com>
2022-05-01 16:59:45 +0700

    update cart-member

---
+++ member/product_detail.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Product Detail</title>
+<link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<table width="600" border="0" align="center" class="square">
+  <tr><td colspan="3" bgcolor="#CCCCCC"><b>Product</b></td></tr>
+  
+<?php
+//connect db
+    include("connect.php");
+	$p_id = $_GET['p_id']; //สร้างตัวแปร p_id เพื่อรับค่า
+	
+	$sql = "SELECT * FROM tbl_product where p_id=$p_id";  //รับค่าตัวแปร p_id ที่ส่งมา
+	$result = mysqli_query($conn, $sql);
+	// $row = mysqli_fetch_array($result);
+	$row_prd = mysqli_fetch_array($result) ;
+	//แสดงรายละเอียด
+	echo "<tr>";
+  	echo "<td width='85' valign='top'><b>Title</b></td>";
+    echo "<td width='279'>" . $row_prd["p_name"] . "</td>";
+    echo "<td width='70' rowspan='4'><img src='img/" . $row_prd["p_img"] . " ' width='100'></td>";
+  	echo "</tr>";
+  	echo "<tr>";
+    echo "<td valign='top'><b>Detail</b></td>";
+    echo "<td>" . $row_prd["p_detail"] . "</td>";
+  	echo "</tr>";
+  	echo "<tr>";
+    echo "<td valign='top'><b>Price</b></td>";
+    echo "<td>" .number_format($row_prd["p_price"],2) . "</td>";
+  	echo "</tr>"; 
+  	echo "<tr>";
+    echo "<td colspan='2' align='center'>";
+    echo "<a href='cart.php?p_id=$row_prd[p_id]&act=add'> เพิ่มลงตะกร้าสินค้า </a></td>";
+    echo "</tr>";
+	
+?>
+</table>
+
+<p><center><a href="product.php">กลับไปหน้ารายการสินค้า</a></center>
+</body>
+</html>
